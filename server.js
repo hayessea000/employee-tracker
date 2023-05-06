5f64ca20857edae7aadbbcb709fddd9148454cdb
Sean Hayes <hayessea000@outlook.com>
2023-05-05 21:09:54 -0500

    set up some inquirer

--- server.js
+++ server.js
@@ -18,9 +18,103 @@ const db = mysql.createConnection(
   console.log(`Connected to the employee_db database.`)
 );
 
+const inquirerQuestions = ["view all departments", "view all roles", "view all employees",
+    "add a department", "add a role", "add an employee", "update an employee role"]
 
-
-
+inquirer
+.prompt([
+    {
+        type: 'list',
+        message: 'What would you like to do?',
+        name: 'starterQ',
+        choices: inquirerQuestions,
+    },
+])
+.then((data) => {
+    if (data.starterQ == "add a department"){
+        inquirer
+        .prompt([
+            {
+                type: 'input',
+                name: 'addDepartment',
+                message: 'What is the name of the department?',
+            },
+        ])
+        .then((data) => {
+            // ????
+        });
+    }else if (data.starterQ == "add a role"){
+        inquirer
+        .prompt([
+            {
+                type: 'input',
+                name: 'roleName',
+                message: 'What is the name of the role?',
+            },
+            {
+                type: 'input',
+                name: 'roleSalary',
+                message: 'What is the salary of the role?',
+            },
+            {
+                type: 'list',
+                message: 'What is the department the role is in?',
+                name: 'roleDepartment',
+                // choices: departments,
+            },
+        ])
+        .then((data) => {
+            // ????
+        });
+    }else if (data.starterQ == "add an employee"){
+        inquirer
+        .prompt([
+            {
+                type: 'input',
+                name: 'fName',
+                message: 'What is their first name?',
+            },
+            {
+                type: 'input',
+                name: 'lName',
+                message: 'What is their last name?',
+            },
+            {
+                type: 'input',
+                name: 'employeeRole',
+                message: 'What is the employees role?',
+            },
+            {
+                type: 'list',
+                message: 'Who is the employees manager?',
+                name: 'employeeMan',
+                // choices: Managers,
+            },
+        ])
+        .then((data) => {
+            // ????
+        });
+    }else if (data.starterQ == "update an employee role"){
+        inquirer
+        .prompt([
+            {
+                type: 'list',
+                message: 'What employee do you want to update?',
+                name: 'employeeName',
+                // choices: employee,
+            },
+            {
+                type: 'list',
+                message: 'What is there new role',
+                name: 'updateRole',
+                // choices: roles,
+            },
+        ])
+        .then((data) => {
+            // ????
+        });
+    }
+});
 
 
 app.use((req, res) => {
