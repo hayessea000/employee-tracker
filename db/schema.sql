c50f3021bb5b756fb29cf5a7bdc3faf61403a525
Sean Hayes <hayessea000@outlook.com>
2023-05-05 20:30:52 -0500

    set up basic tables

--- db/schema.sql
+++ db/schema.sql
@@ -3,16 +3,27 @@ CREATE DATABASE employee_db;
 
 USE employee_db;
 
--- CREATE TABLE movies (
---   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
---   movie_name VARCHAR(100) NOT NULL
--- );
+CREATE TABLE department (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  department_name VARCHAR(30) NOT NULL
+);
 
--- CREATE TABLE reviews (
---     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
---     movie_id INT,
---     review TEXT NOT NULL,
---     FOREIGN KEY (movie_id)
---     REFERENCES movies(id)
---     ON DELETE SET NULL
--- );
+CREATE TABLE role (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL,
+    department_id INT,
+        FOREIGN KEY (department_id)
+        REFERENCES department(id)
+    -- ON DELETE SET NULL
+);
+
+CREATE TABLE employee (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  department_id INT,
+    FOREIGN KEY (role_id)
+    REFERENCES role(id)
+--   manager_id INT
+);
--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const mysql = require('mysql2');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 const db = mysql.createConnection(
