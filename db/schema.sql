0dd90372d02c5e58ba0d99c337e3445f3db207f8
Sean Hayes <hayessea000@outlook.com>
2023-05-05 19:45:59 -0500

    first commit

---
+++ db/schema.sql
@@ -0,0 +1,18 @@
+DROP DATABASE IF EXISTS employee_db;
+CREATE DATABASE employee_db;
+
+USE employee_db;
+
+-- CREATE TABLE movies (
+--   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+--   movie_name VARCHAR(100) NOT NULL
+-- );
+
+-- CREATE TABLE reviews (
+--     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+--     movie_id INT,
+--     review TEXT NOT NULL,
+--     FOREIGN KEY (movie_id)
+--     REFERENCES movies(id)
+--     ON DELETE SET NULL
+-- );
