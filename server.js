b03b20c7ab669847190333f0f18b7bd9a70f0ccd
Sean Hayes <hayessea000@outlook.com>
2023-05-06 22:34:23 -0500

    finshed getting data to add employee

--- server.js
+++ server.js
@@ -12,125 +12,132 @@ const db = mysql.createConnection(
 );
 
 const inquirerQuestions = ["view all departments", "view all roles", "view all employees",
-    "add a department", "add a role", "add an employee", "update an employee role"]
+    "add a department", "add a role", "add an employee", "update an employee role"
+]
 
-inquirer
-.prompt([
-    {
-        type: 'list',
-        message: 'What would you like to do?',
-        name: 'starterQ',
-        choices: inquirerQuestions,
-    },
-])
-.then((data) => {
-    if (data.starterQ == "add a department"){
-        inquirer
-        .prompt([
-            {
-                type: 'input',
-                name: 'addDepartment',
-                message: 'What is the name of the department?',
-            },
-        ])
-        .then((data) => {
-            // ????
-        });
-    }else if (data.starterQ == "add a role"){
-        inquirer
-        .prompt([
-            {
-                type: 'input',
-                name: 'roleName',
-                message: 'What is the name of the role?',
-            },
-            {
-                type: 'input',
-                name: 'roleSalary',
-                message: 'What is the salary of the role?',
-            },
-            {
-                type: 'list',
-                message: 'What is the department the role is in?',
-                name: 'roleDepartment',
-                // choices: departments,
-            },
-        ])
-        .then((data) => {
-            // ????
-        });
-    }else if (data.starterQ == "add an employee"){
-        db.query("SELECT * FROM role",(err,res)=>{
-            if (err) throw err
-            console.log(res)
-            let roleChoices= res.map(({id, title})=>({
-                name:title,
-                value:id,
-            }))
+let runFunction = function(){
+    inquirer
+    .prompt([
+        {
+            type: 'list',
+            message: 'What would you like to do?',
+            name: 'starterQ',
+            choices: inquirerQuestions,
+        },
+    ])
+    .then((data) => {
+        if (data.starterQ == "add a department"){
             inquirer
             .prompt([
                 {
                     type: 'input',
-                    name: 'fName',
-                    message: 'What is their first name?',
-                },
-                {
-                    type: 'input',
-                    name: 'lName',
-                    message: 'What is their last name?',
-                },
-                {
-                    type: 'list',
-                    name: 'employeeRole',
-                    message: 'What is the employees role?',
-                    choices: roleChoices
+                    name: 'addDepartment',
+                    message: 'What is the name of the department?',
                 },
             ])
             .then((data) => {
-                console.log(data)
-                let addEmployeeOne = data
-                db.query("SELECT * FROM employee",(err,res)=>{
-                    if (err) throw err
-                    console.log(res)
-                    let managerChoices= res.map(({id, first_name, last_name})=>({
-                        name:`${first_name} ${last_name}`,
-                        value:id,
-                    }))
-                    inquirer
-                    .prompt([
-                        {
-                            type: 'list',
-                            message: 'Who is the employees manager?',
-                            name: 'employeeMan',
-                            choices: managerChoices,
-                        },
-                    ])
-                    .then((data) => {
-                        let addEmployeeFull = { first_name: addEmployeeOne.fName, last_name: addEmployeeOne.lName, role_id: addEmployeeOne.employeeRole, manager: data.employeeMan}
-                        console.log(addEmployeeFull)
+                // ????
+            });
+        }else if (data.starterQ == "add a role"){
+            inquirer
+            .prompt([
+                {
+                    type: 'input',
+                    name: 'roleName',
+                    message: 'What is the name of the role?',
+                },
+                {
+                    type: 'input',
+                    name: 'roleSalary',
+                    message: 'What is the salary of the role?',
+                },
+                {
+                    type: 'list',
+                    message: 'What is the department the role is in?',
+                    name: 'roleDepartment',
+                    // choices: departments,
+                },
+            ])
+            .then((data) => {
+                // ????
+            });
+        }else if (data.starterQ == "add an employee"){
+            db.query("SELECT * FROM role",(err,res)=>{
+                if (err) throw err
+                console.log(res)
+                let roleChoices= res.map(({id, title})=>({
+                    name:title,
+                    value:id,
+                }))
+                inquirer
+                .prompt([
+                    {
+                        type: 'input',
+                        name: 'fName',
+                        message: 'What is their first name?',
+                    },
+                    {
+                        type: 'input',
+                        name: 'lName',
+                        message: 'What is their last name?',
+                    },
+                    {
+                        type: 'list',
+                        name: 'employeeRole',
+                        message: 'What is the employees role?',
+                        choices: roleChoices
+                    },
+                ])
+                .then((data) => {
+                    console.log(data)
+                    let addEmployeeOne = data
+                    db.query("SELECT * FROM employee WHERE manager IS NULL",(err,res)=>{
+                        if (err) throw err
+                        console.log(res)
+                        let managerChoices= res.map(({id, first_name, last_name})=>({
+                            name:`${first_name} ${last_name}`,
+                            value:id,
+                        }))
+                        inquirer
+                        .prompt([
+                            {
+                                type: 'list',
+                                message: 'Who is the employees manager?',
+                                name: 'employeeMan',
+                                choices: managerChoices,
+                            },
+                        ])
+                        .then((data) => {
+                            let addEmployeeFull = { first_name: addEmployeeOne.fName, last_name: addEmployeeOne.lName, role_id: addEmployeeOne.employeeRole, manager: data.employeeMan}
+                            console.log(addEmployeeFull)
+                            // write in data
+                        });
                     });
                 });
+            })
+            
+        }else if (data.starterQ == "update an employee role"){
+            inquirer
+            .prompt([
+                {
+                    type: 'list',
+                    message: 'What employee do you want to update?',
+                    name: 'employeeName',
+                    // choices: employee,
+                },
+                {
+                    type: 'list',
+                    message: 'What is there new role',
+                    name: 'updateRole',
+                    // choices: roles,
+                },
+            ])
+            .then((data) => {
+                // ????
             });
-        })
-        
-    }else if (data.starterQ == "update an employee role"){
-        inquirer
-        .prompt([
-            {
-                type: 'list',
-                message: 'What employee do you want to update?',
-                name: 'employeeName',
-                // choices: employee,
-            },
-            {
-                type: 'list',
-                message: 'What is there new role',
-                name: 'updateRole',
-                // choices: roles,
-            },
-        ])
-        .then((data) => {
-            // ????
-        });
-    }
-});
+        }
+    });
+    runFunction()
+}
+
+runFunction()
